736f2c83d543114cd9f02a1eb027ad70b732d1c9
mmoha-100 <g1thubm0haammed@gmail.com>
2025-02-08 07:48:50 +0300

    Update mian.js To Control Back To Top Button

--- Page Scroll Progress/mian.js
+++ Page Scroll Progress/mian.js
@@ -17,3 +17,23 @@ window.addEventListener("scroll", () => {
     let scrollTop = document.documentElement.scrollTop;
     scroller.style.width = `${(scrollTop / height) * 100}%`; // To Calculate The Ratio
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        backToTopBtn.style.opacity = "1";
+    } else {
+        backToTopBtn.style.opacity = "0";
+    }
+};
+
+// When the user clicks the button, scroll to the top of the document
+backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
